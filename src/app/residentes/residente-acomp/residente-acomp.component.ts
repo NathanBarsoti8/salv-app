f6ee192e77dbe12d77b3944655540bb01aae45b3
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-05-26 12:39:04 -0300

    animation residente-acomp

--- src/app/residentes/residente-acomp/residente-acomp.component.ts
+++ src/app/residentes/residente-acomp/residente-acomp.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'salv-residente-acomp',
-  templateUrl: './residente-acomp.component.html'
+  templateUrl: './residente-acomp.component.html',
+  animations: [
+    trigger('residente-acompAppeared', [
+        state('ready', style({ opacity: 1 })),
+        transition('void => ready', [
+            style({ opacity: 0, transform: 'translate(-30px, -10px)' }),
+            animate('500ms 0s ease-in-out')
+        ])
+    ])
+  ]
 })
+
 export class ResidenteAcompComponent implements OnInit {
+
+  residenteacompState = 'ready'
 
   constructor() { }
 
